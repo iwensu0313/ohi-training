0725934f323fed10f3102ec6beb94ce35407736b
iwensu0313 <isu@bren.ucsb.edu>
2018-06-03 10:27:20 -0700

    added spatial training doc

---
+++ spatialtraining.R
@@ -0,0 +1,10 @@
+## Spatial Training
+
+## install packages
+install.packages(c("sf","raster","rgdal"))
+
+## load packages using library()
+library(tidyverse)
+library(sp)
+library(rgdal)
+library(sf)
